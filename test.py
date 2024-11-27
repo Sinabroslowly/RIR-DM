708a2f9395648fce44bf77bce8a79029327c3382
Minjae Kim <102868422+Sinabroslowly@users.noreply.github.com>
2024-11-27 13:11:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ from scripts.dataset import RIRDDMDataset
 from scripts.model import ConditionalDDPM
 from scripts.stft import STFT
 
-NUM_SAMPLE_STEPS = 50
+NUM_SAMPLE_STEPS = 200
 
 def save_t60_analysis(examples, t60_err, t60_vals, output_dir, version):
     """
