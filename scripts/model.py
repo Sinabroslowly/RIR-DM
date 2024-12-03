3ab7a7c8c8d995cce046076669c0e01e62f8ae3e
Minjae Kim <102868422+Sinabroslowly@users.noreply.github.com>
2024-12-03 16:33:16 +0900

    Update model.py

--- scripts/model.py
+++ scripts/model.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from diffusers import EDMDPMSolverMultistepScheduler, SD3Transformer2DModel
+from diffusers.configuration_utils import ConfigMixIn, register_to_config
+from diffusers.modeling_utils import ModelMixIn
 
 class FeatureMapGenerator(nn.Module):
     def __init__(self):
@@ -28,6 +30,7 @@ class LDT(ModelMixIn, ConfigMixIn):
         num_attention_heads = 8,
         joint_attention_dim=4096,
         patch_size=2,
+        num_train_timesteps=1000,
     ):
         super().__init__()
 
@@ -44,6 +47,14 @@ class LDT(ModelMixIn, ConfigMixIn):
         )
 
         self.cross_modal_embedding = nn.Linear(cross_attention_dim, joint_attention_dim)
+
+        self.scheduler = EDMDPMSolverMultistepScheduler(sigma_min=0.002, 
+                                                        sigma_max=80.0, 
+                                                        sigma_data=0.5,
+                                                        sigma_schedule='karras',
+                                                        solver_order=2,
+                                                        prediction_type='epsilon',
+                                                        num_train_timesteps=num_train_timesteps) # Noise scheduler
 
     def forward(self, latent_input, cross_modal_embedding=None, timestep=None):
         if cross_modal_embedding is not None:
@@ -65,30 +76,33 @@ def print_gpu_utilization():
 
 def main():
     # Example inputs
-    batch_size = 1
-    noise_channels = 1
-    conditional_channels = 1
-    image_size = 512
+    batch_size = 4
+    noise_channels = 16
+    image_size = 32
+    embedding_size = 512
     device = torch.cuda.current_device() if torch.cuda.is_available() else "cpu"
     print(f"Device: {device}")
 
     # Initialize the model
     model = LDT().to(device)
+    featuremap_generator = FeatureMapGenerator()
 
     print(f"The model has {sum(p.numel() for p in model.parameters())} parameters")
 
     # Noise tensor (noisy spectrogram)
-    noisy_spectrogram = torch.randn(batch_size, noise_channels, image_size, image_size).to(device)
+    latent_input_test = torch.randn(batch_size, noise_channels, image_size, image_size).to(device)
 
     # Condition tensor: [batch, conditional_channels, 512, 512] (feature map from shared embedding)
-    image_embedding = torch.randn(batch_size, image_size).to(device)
-    text_embedding = torch.randn(batch_size, image_size).to(device)
+    image_embedding = torch.randn(batch_size, embedding_size).to(device)
+    text_embedding = torch.randn(batch_size, embedding_size).to(device)
+
+    cross_modal_embedding = featuremap_generator(text_embedding, image_embedding)
 
     # Timestep (current diffusion timestep)
-    timestep = torch.randint(0, model.scheduler.config.num_train_timesteps, (batch_size,), device=noisy_spectrogram.device)
+    timestep = torch.randint(0, model.scheduler.config.num_train_timesteps, (batch_size,), device=latent_input_test.device)
 
     # Forward pass
-    denoised_output = model(noisy_spectrogram, timestep, image_embedding, text_embedding)
+    denoised_output = model(latent_input_test, cross_modal_embedding, timestep)
 
     print_gpu_utilization()
 
