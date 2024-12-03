3ad1744efa9c5efa515252d29add9aaf52ca12ca
Minjae Kim <102868422+Sinabroslowly@users.noreply.github.com>
2024-12-03 16:59:03 +0900

    Update model.py

--- scripts/model.py
+++ scripts/model.py
@@ -18,15 +18,15 @@ class FeatureMapGenerator(nn.Module):
 class LDT(nn.Module):
     def __init__(
         self,
-        sample_size=32,
-        in_channels=16,
-        out_channels=16,
-        cross_attention_dim=512,
+        sample_size=32, # latent size of VQ-VAE representation (Five downsampling: 512-256-128-64-32)
+        in_channels=16, # Dimension of VQ-VAE latent representation.
+        out_channels=16, # Dimension of VQ-VAE latent representation.
+        cross_attention_dim=512, # Added
         num_layers=6, # Default 18
-        attention_head_dim=64,
-        num_attention_heads = 8,
-        joint_attention_dim=4096,
-        patch_size=2,
+        attention_head_dim=64, # Default 64
+        num_attention_heads = 8, # Default 18
+        joint_attention_dim=512, # Default 4096
+        patch_size=2, # Default 2
         num_train_timesteps=1000,
     ):
         super().__init__()
@@ -41,8 +41,8 @@ class LDT(nn.Module):
             num_attention_heads=num_attention_heads,
             joint_attention_dim=joint_attention_dim,
         )
-        self.num_blocks = self.num_layers
-        self.inner_dim = self.num_attention_heads * self.attention_head_dim
+        self.num_blocks = num_layers
+        self.inner_dim = num_attention_heads * attention_head_dim
 
         self.cross_modal_embedding = nn.Linear(cross_attention_dim, joint_attention_dim)
 
@@ -59,8 +59,10 @@ class LDT(nn.Module):
     def forward(self, latent_input, cross_modal_embedding=None, timestep=None):
         if cross_modal_embedding is not None:
           cross_modal_embedding = self.cross_modal_embedding(cross_modal_embedding)
+          print(f"Shape of cross_modal_embedding after projection: {cross_modal_embedding.shape}")
 
           control_states = self.controlnet_proj(cross_modal_embedding)
+          print(f"Shape of control_states after projection: {control_states.shape}")
           control_states = control_states.view(latent_input.shape[0],self.num_blocks, self.inner_dim, latent_input.shape[2], latent_input.shape[3])
 
           block_controlnet_hidden_states = torch.split(control_states, 1, dim=1)
@@ -100,12 +102,14 @@ def main():
 
     # Noise tensor (noisy spectrogram)
     latent_input_test = torch.randn(batch_size, noise_channels, image_size, image_size).to(device)
+    print(f"Shape of the latent_input_test: {latent_input_test.shape}")
 
     # Condition tensor: [batch, conditional_channels, 512, 512] (feature map from shared embedding)
     image_embedding = torch.randn(batch_size, embedding_size).to(device)
     text_embedding = torch.randn(batch_size, embedding_size).to(device)
 
     cross_modal_embedding = featuremap_generator(text_embedding, image_embedding)
+    print(f"Shape of the cross_modal_embedding: {cross_modal_embedding.shape}")
 
     # Timestep (current diffusion timestep)
     timestep = torch.randint(0, model.scheduler.config.num_train_timesteps, (batch_size,), device=latent_input_test.device)
